1215dc01a05d113408febb121660d98e1b9c4fc1
josearizaga009 <josearizaga009@gmail.com>
2023-12-29 18:42:15 -0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	renamed:    dist/assets/index-oCu-Wrd_.css -> dist/assets/index-6mIxlJfo.css
    	renamed:    dist/assets/index-jDkuQYIY.js -> dist/assets/index-R5HLI7or.js
    	new file:   dist/fonts/Ubuntu-BI.ttf
    	new file:   dist/fonts/Ubuntu-C.ttf
    	new file:   dist/fonts/Ubuntu-L.ttf
    	modified:   dist/index.html
    	new file:   public/fonts/Ubuntu-BI.ttf
    	new file:   public/fonts/Ubuntu-C.ttf
    	new file:   public/fonts/Ubuntu-L.ttf
    	modified:   src/index.css
    	modified:   src/views/Login.css
    	modified:   src/views/Login.jsx

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,5 +1,50 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import AnimateElement from "../components/AnimateElement";
 import "./Login.css";
+import { faLock, faUser } from "@fortawesome/free-solid-svg-icons";
 
 export default function Login() {
-    return "LOGIN";
+    return (
+        <AnimateElement className="login-page">
+            <form
+                action="#"
+                onSubmit={(evt) => evt.preventDefault()}
+                id="element_form"
+            >
+                <div className="container">
+                    <img className="logo" src="./img/logo.png" alt="Logo" />
+                    <span>Cursus ISTS</span>
+                    <div className="input">
+                        <FontAwesomeIcon icon={faUser} />
+                        <input
+                            placeholder="Usuario"
+                            type="text"
+                            name="user_user"
+                        />
+                    </div>
+                    <div className="input">
+                        <FontAwesomeIcon icon={faLock} />
+                        <input
+                            placeholder="Contraseña"
+                            type="password"
+                            name="user_pass"
+                        />
+                    </div>
+                    <div className="input msg">
+                        <span className="msg"></span>
+                    </div>
+                    <div className="input submit">
+                        <input type="submit" value="Iniciar sesion" />
+                    </div>
+                </div>
+            </form>
+
+            <section className="ideamodal _open" id="sectionProgress">
+                <div className="modal-progress">
+                    <span id="sectionProgressText">Procesando..</span>
+                    <div className="progress_bar"></div>
+                </div>
+            </section>
+        </AnimateElement>
+    );
 }
